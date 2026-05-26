836b668c27bf27e2ada547a2314cd8a40c998efe
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-05-26 18:23:24 -0300

    Correcao Inserção de usuário com POST

--- src/main/java/com/dariomonteiro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/dariomonteiro/workshopmongo/resources/UserResource.java
@@ -8,11 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-=======
->>>>>>> 6359cc38d06417d8a35b9af34150a6784c5d4905
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -38,7 +35,6 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
-<<<<<<< HEAD
 	}
 	
 	@PostMapping
@@ -48,7 +44,4 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-=======
-	}	
->>>>>>> 6359cc38d06417d8a35b9af34150a6784c5d4905
 }
